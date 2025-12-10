8fd239597858cbafc0684664b914407397993a5e
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-12-10 10:58:03 +0100

    feat: provide many combinaisons
    The failing ones are all with `mimetype` entryName, with `extendedTimestamp: false`

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,48 @@
+import { spawn } from 'node:child_process';
+import { once } from 'node:events';
 import { writeFile } from 'node:fs/promises';
+import { join, relative } from 'node:path';
 
 import { BlobWriter, TextReader, ZipWriter } from '@zip.js/zip.js';
 
-import { isMimetype } from './signature_check.ts';
+const entryNames = ['mimetype', 'gibberish'];
+const compressionMethods = [0, 8];
+const mimetypes = ['application/zip', 'application/x-nmrium+zip'];
+const extendedTimestamps = [true, false];
 
-async function working() {
-  const filename = 'macos-working-but-not-signature-compatible.zip';
-  const zipWriter = new ZipWriter(new BlobWriter());
-
-  const entry = await zipWriter.add(
-    'mimetype',
-    new TextReader('application/zip'),
-    {
-      compressionMethod: 0,
-      extendedTimestamp: false, // smaller payload, the extra field is empty
-    },
-  );
-
-  const blob = await zipWriter.close();
-  await writeFile(filename, blob.stream());
-
-  console.log(filename, 'written');
-
-  const arrayBuffer = await blob.arrayBuffer();
-  const matchSignature = isMimetype(arrayBuffer, 'application/zip');
-
-  const headerPlusDataSize = 53;
-  const dataDescriptorSize = 12;
-  const nextPossibleHeaderSize = 4;
-  const buffer = Buffer.from(arrayBuffer);
-  const bufferSlice = buffer
-    .subarray(
-      0,
-      headerPlusDataSize +
-        entry.rawExtraField.byteLength +
-        dataDescriptorSize +
-        nextPossibleHeaderSize,
-    )
-    .toString('hex');
-
-  console.log(
-    `working: true, matchSignature: ${matchSignature}\nbufferSlice: ${bufferSlice}\nbuffer: ${buffer.toString('hex')}`,
-  );
+for (const extendedTimestamp of extendedTimestamps) {
+  for (const entryName of entryNames) {
+    for (const mimetype of mimetypes) {
+      for (const compressionMethod of compressionMethods) {
+        // eslint-disable-next-line no-await-in-loop
+        await test(entryName, mimetype, compressionMethod, extendedTimestamp);
+      }
+    }
+  }
 }
 
-async function notWorking() {
-  const filename = 'macos-not-working-but-signature-check-compatible.zip';
+async function test(
+  entryName: string,
+  mimetype: string,
+  compressionMethod: number,
+  extendedTimestamp: boolean,
+) {
   const zipWriter = new ZipWriter(new BlobWriter());
 
-  const entry = await zipWriter.add(
-    'mimetype',
-    new TextReader('application/zip'),
-    {
-      compressionMethod: 0,
-      // ensures the data length is written in the local file header,
-      // but it seems macOS struggle to decode if the data descriptor is missing.
-      dataDescriptor: false,
-      extendedTimestamp: false, // smaller payload, the extra field is empty
-    },
-  );
+  await zipWriter.add(entryName, new TextReader(mimetype), {
+    compressionMethod,
+    extendedTimestamp,
+  });
 
   const blob = await zipWriter.close();
-  await writeFile(filename, blob.stream());
 
-  console.log(filename, 'written');
-
-  const arrayBuffer = await blob.arrayBuffer();
-  const matchSignature = isMimetype(arrayBuffer, 'application/zip');
-
-  const headerPlusDataSize = 53;
-  const dataDescriptorSize = 12;
-  const nextPossibleHeaderSize = 4;
-  const buffer = Buffer.from(arrayBuffer);
-  const bufferSlice = buffer
-    .subarray(
-      0,
-      headerPlusDataSize +
-        entry.rawExtraField.byteLength +
-        dataDescriptorSize +
-        nextPossibleHeaderSize,
-    )
-    .toString('hex');
-
-  console.log(
-    `working: false, matchSignature: ${matchSignature}\nbufferSlice: ${bufferSlice}\nbuffer: ${buffer.toString('hex')}`,
+  const filename = `${entryName}-${mimetype.split('/')[1]}-${compressionMethod}-${extendedTimestamp}.zip`;
+  const filePath = relative(
+    process.cwd(),
+    join(import.meta.dirname, '../zips', filename),
   );
-}
+  await writeFile(filePath, blob.stream());
 
-await working();
-console.log();
-await notWorking();
+  const fileProcess = spawn('file', [filePath], { stdio: 'inherit' });
+  await once(fileProcess, 'close');
+}
